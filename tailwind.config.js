f0c7b071f5d216f7ed5437e218c3543a3dab424e
Saeid Soodi <saeid.Soodi1998@gmail.com>
2024-04-09 02:44:53 +0330

    learned to add custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./public/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "primary":"#660000"
+      }
+    },
   },
   plugins: [],
 }
